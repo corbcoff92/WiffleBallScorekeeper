c5522af88eb73f2a0626b918c1db9046fca8b25e
Corbyn Coffin <corbcoff92@gmail.com>
2022-01-13 12:37:55 -0700

    Moved MainMenu class to menu package

---
+++ menu/MainMenu.java
@@ -0,0 +1,33 @@
+package menu;
+
+import main.WiffleBallScorekeeper;
+
+public class MainMenu extends Menu
+{
+    private WiffleBallScorekeeper scorekeeperApp;
+    
+    public MainMenu(WiffleBallScorekeeper scorekeeperApp)
+    {
+        super("N - New Game", "Q - Quit");
+        this.scorekeeperApp = scorekeeperApp;
+    }
+
+    protected void display()
+    {
+        System.out.println("Main Menu");
+        super.display();
+    }
+
+    protected void processInput(char input)
+    {
+        switch (input)
+        {
+            case 'N':
+                scorekeeperApp.newGame();
+                break;
+            case 'Q':
+                scorekeeperApp.exit = true;
+                break;
+        }
+    }    
+}
