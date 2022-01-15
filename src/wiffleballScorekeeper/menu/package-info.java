4e118d57551d2dd5493446c10d45ca1f46b8f4ab
Corbyn Coffin <corbcoff92@gmail.com>
2022-01-15 12:25:33 -0700

    Added package-level document comments

---
+++ src/wiffleballScorekeeper/menu/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the classes used to create all of the menus for the Wiffleball Scorekeeper app. 
+ * It contains an abstract base class {@link Menu} that is used for all menus of the application. 
+ * This base class also contains some static methods useful for working with the console.
+ * @author Corbyn Coffin
+ */
+package wiffleballScorekeeper.menu;
---
+++ src/wiffleballScorekeeper/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains a collection of classes used to produce an implementation of an app that 
+ * functions as a scorekeeper for a game of wiffleball.
+ * @author Corbyn Coffin
+ */
+package wiffleballScorekeeper;
